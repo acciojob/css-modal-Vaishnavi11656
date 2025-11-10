cbb786a90888120596a62a5b892b39340ec0b1f6
Vaishnavi11656 <166276503+Vaishnavi11656@users.noreply.github.com>
2025-11-10 23:48:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const modal = document.getElementByID("myModal");
+const openBtn = document.getElementByID("openModal");
+const closeBtn = document.getElementByID(".close-modal");
+
+
+openBtn.onClick = function(){
+	modal.style.display = "flex";
+}
+
+closeBtn.onClick = function (){
+	modal.style.display = "none";
+}
+
+modal.onClick = function(event){
+	if(event.target == modal){
+		modal.style.display = "none";
+	}
+}
